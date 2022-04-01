a218cfbf8e608e7579f10d88ee1753c1fadfa9fd
MouazAboYazan <87276813+MouazAboYazan@users.noreply.github.com>
2022-04-01 19:35:40 +0300

    Update countdown.js

--- assets/countdown.js
+++ assets/countdown.js
@@ -1,4 +1,4 @@
-let countDownDate = new Date("April 1, 2022 23:59:59").getTime();
+let countDownDate = new Date("April 2, 2022 23:59:59").getTime();
 let counter = setInterval(() => {
     let dateNow = new Date().getTime();
     let dateDiff = countDownDate - dateNow;
